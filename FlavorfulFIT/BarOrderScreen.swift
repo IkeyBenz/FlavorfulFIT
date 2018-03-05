cc4914107e0c08e5993b88f2c25e1a7206f4da08
Ikey Benzaken <ikey.benz@gmail.com>
2018-03-05 03:20:10 -0500

    Polish
    Dedicated HUD that stays throughout all pages
    Various constraint fixes
    
    Next Up:
    Paypal Implementation
    Little More Polish

--- FlavorfulFIT/BarOrderScreen.swift
+++ FlavorfulFIT/BarOrderScreen.swift
@@ -27,21 +27,21 @@ class BarOrderScreen: UIViewController {
         switch tag {
             case 1:
                 title = "Coconut Marzipan"
-                descriptionLabel.text = "Decadent bar with coconut and almond flavor profile. Chocolate chips sprinkled throughout. A full nutritious meal at 200 calories with only 2 grams of sugar. Grain free. Take one with water, keep frozen.\n*Contains tree nuts. Processed on equipment that handles peanuts. \nCertified Kosher Parve. Pack of 7."
+                descriptionLabel.text = "Decadent bar with coconut and almond flavor profile. Chocolate chips sprinkled throughout. A full nutritious meal at 200 calories with only 2 grams of sugar. Grain free. Take one with water, keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- Pack of 7."
             break
             case 2:
                 title = "Berries & Cream"
-                descriptionLabel.text = "Sweet vanilla base with a burst of cranberry in every bite. A full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n*Contains tree nuts. Processed on equipment that handles peanuts.\nCertified Kosher Parve. Pack of 7."
+                descriptionLabel.text = "Sweet vanilla base with a burst of cranberry in every bite. A full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- Pack of 7."
                 barImg.image = UIImage(named: "BerriesnCream")
             break
             case 3:
                 title = "Chocolate Peanut Butter"
-                descriptionLabel.text = "Delicious fudgey peanut butter bar. Crunchy peanuts and chocolate chips sprinkled throughout. A full nutritious meal at 200 calories with only 2 grams sugar. Grain free. Take one with water. Keep frozen.\n*Contains peanuts and tree nuts.\nCertified Kosher Parve. Pack of 7."
+                descriptionLabel.text = "Delicious fudgey peanut butter bar. Crunchy peanuts and chocolate chips sprinkled throughout. A full nutritious meal at 200 calories with only 2 grams sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains peanuts and tree nuts.\n- Certified Kosher Parve.\n- Pack of 7."
                 barImg.image = UIImage(named: "ChocoPeanut")
             break
             case 4:
                 title = "Chocolate Brownie"
-                descriptionLabel.text = "Rich chocolate brownie loaded with chocolate chips and deep cocoa flavor. A full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n*Contains tree nuts. Processed on equipment that handles peanuts.\nCertified Kosher Parve. Pack of 7."
+                descriptionLabel.text = "Rich chocolate brownie loaded with chocolate chips and deep cocoa flavor. A full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- Pack of 7."
             break
             case 5:
                 title = "Assorted Chocolate"
@@ -49,12 +49,12 @@ class BarOrderScreen: UIViewController {
             break
             case 6:
                 title = "Assorted Vanilla";
-                descriptionLabel.text = "Assorted box of vanilla-based FlavorfulFIT Meal Replacement Bars (Coconut Marzipan and Berries & Cream). Each one is a full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n*Contains tree nuts. Processed on equipment that handles peanuts.\nCertified Kosher Parve. Pack of 7."
+                descriptionLabel.text = "Assorted box of vanilla-based FlavorfulFIT Meal Replacement Bars (Coconut Marzipan and Berries & Cream). Each one is a full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- Pack of 7."
                 barImg.image = UIImage(named: "AssortedVanilla")
             break
             case 7:
                 title = "Sesame Crumb Coating"
-                descriptionLabel.text = "Crunchy seasoned grainless 'breadcrumbs' with sesame. Use on anything from chicken, to fish, to veggies. Amazing baked or air fried! One pound container.\n*Contains tree nuts. Processed on equipment that handles peanuts.\nCertified Kosher Parve. Pack of 7."
+                descriptionLabel.text = "Crunchy seasoned grainless 'breadcrumbs' with sesame. Use on anything from chicken, to fish, to veggies. Amazing baked or air fried! One pound container.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- Pack of 7."
                 barImg.image = UIImage(named: "CrumbsImg")
             default: title = ""
         }
