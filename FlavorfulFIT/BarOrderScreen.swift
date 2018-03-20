dc1c235d0893e431d018652717489fd9372062e7
Ikey Benzaken <ikey.benz@gmail.com>
2018-03-19 20:18:00 -0400

    Polish

--- FlavorfulFIT/BarOrderScreen.swift
+++ FlavorfulFIT/BarOrderScreen.swift
@@ -45,43 +45,43 @@ class BarOrderScreen: UIViewController {
         switch tag {
             case 1:
                 title = "Coconut Marzipan"
-                descriptionLabel.text = "Decadent bar with coconut and almond flavor profile. Chocolate chips sprinkled throughout. A full nutritious meal at 200 calories with only 2 grams of sugar. Grain free. Take one with water, keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- Pack of 7."
+                descriptionLabel.text = "Decadent bar with coconut and almond flavor profile. Chocolate chips sprinkled throughout. A full nutritious meal at 200 calories with only 2 grams of sugar. Grain free. Take one with water, keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- $15 for a pack of 7."
                 productImageView.image = UIImage(named: "CoconutMarzipan")
                 price = 15.00
             break
             case 2:
                 title = "Berries & Cream"
-                descriptionLabel.text = "Sweet vanilla base with a burst of cranberry in every bite. A full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- Pack of 7."
+                descriptionLabel.text = "Sweet vanilla base with a burst of cranberry in every bite. A full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- $15 for a pack of 7."
                 productImageView.image = UIImage(named: "BerriesnCream")
                 price = 15.00
             break
             case 3:
                 title = "Chocolate Peanut Butter"
-                descriptionLabel.text = "Delicious fudgey peanut butter bar. Crunchy peanuts and chocolate chips sprinkled throughout. A full nutritious meal at 200 calories with only 2 grams sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains peanuts and tree nuts.\n- Certified Kosher Parve.\n- Pack of 7."
+                descriptionLabel.text = "Delicious fudgey peanut butter bar. Crunchy peanuts and chocolate chips sprinkled throughout. A full nutritious meal at 200 calories with only 2 grams sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains peanuts and tree nuts.\n- Certified Kosher Parve.\n- $15 for a pack of 7."
                 productImageView.image = UIImage(named: "ChocoPeanut")
                 price = 15.00
             break
             case 4:
                 title = "Chocolate Brownie"
-                descriptionLabel.text = "Rich chocolate brownie loaded with chocolate chips and deep cocoa flavor. A full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- Pack of 7."
+                descriptionLabel.text = "Rich chocolate brownie loaded with chocolate chips and deep cocoa flavor. A full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- $15 for a pack of 7."
                 productImageView.image = UIImage(named: "ChocoBrownie")
                 price = 15.00
             break
             case 5:
                 title = "Assorted Chocolate"
-                descriptionLabel.text = "Assorted box of chocolate-based FlavorfulFIT Meal Replacement Bars (Chocolate Peanut Butter and Chocolate Brownie). Each one is a full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains peanuts and tree nuts.\n- Certified Kosher Parve.\n- Pack of 7."
+                descriptionLabel.text = "Assorted box of chocolate-based FlavorfulFIT Meal Replacement Bars (Chocolate Peanut Butter and Chocolate Brownie). Each one is a full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains peanuts and tree nuts.\n- Certified Kosher Parve.\n- $15 for a pack of 7."
                 productImageView.image = UIImage(named: "AssortedChocolate")
                 price = 15.00
             break
             case 6:
                 title = "Assorted Vanilla";
-                descriptionLabel.text = "Assorted box of vanilla-based FlavorfulFIT Meal Replacement Bars (Coconut Marzipan and Berries & Cream). Each one is a full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- Pack of 7."
+                descriptionLabel.text = "Assorted box of vanilla-based FlavorfulFIT Meal Replacement Bars (Coconut Marzipan and Berries & Cream). Each one is a full nutritious meal at 200 calories, with only 2 grams of sugar. Grain free. Take one with water. Keep frozen.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- $15 for a pack of 7."
                 productImageView.image = UIImage(named: "AssortedVanilla")
                 price = 15.00
             break
             case 7:
                 title = "Sesame Crumb Coating"
-                descriptionLabel.text = "Crunchy seasoned grainless 'breadcrumbs' with sesame. Use on anything from chicken, to fish, to veggies. Amazing baked or air fried! One pound container.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- Pack of 7."
+                descriptionLabel.text = "Crunchy seasoned grainless 'breadcrumbs' with sesame. Use on anything from chicken, to fish, to veggies. Amazing baked or air fried! One pound container.\n\n*Contains tree nuts.\n*Processed on equipment that handles peanuts.\n- Certified Kosher Parve.\n- $15 for a pack of 7."
                 productImageView.image = UIImage(named: "CrumbsImg")
             break
             case 8:
@@ -110,9 +110,6 @@ class BarOrderScreen: UIViewController {
     @IBAction func orderButtonPressed(_ sender: Any) {
         if nameTF.text != "" && emailTF.text != "" && phoneTF.text != "" {
             showDropIn(clientTokenOrTokenizationKey: self.clientToken)
-            submitInfoToGoogleForm(name: nameTF.text!, email: emailTF.text!, phone: phoneTF.text!, packageOrdered: productTitle.text!)
-            
-            // Send to thank you screen
         } else {
             let alert = UIAlertController(title: "Form not properly filled out", message: "Please fill out form and try again.", preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
@@ -219,9 +216,22 @@ extension BarOrderScreen {
         
         URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
             if error != nil {
-                print(error)
+                var messageAddon = ""
+                if Singleton.sharedInstance.requestedBarTag <= 8 {
+                    messageAddon = "Your package will be ready for pickup on Sunday."
+                } else {
+                    messageAddon = "Check your email for your two-week program."
+                }
+                let alert = UIAlertController(title: "Success!", message: "Thanks for purchasing \(self.productTitle.text). \(messageAddon)", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                self.submitInfoToGoogleForm(name: self.nameTF.text!, email: self.emailTF.text!, phone: self.phoneTF.text!, packageOrdered: self.productTitle.text!)
+            } else {
+                let alert = UIAlertController(title: "Error!", message: "Something went wrong. Please ensure your credit card details are correct.", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
             }
-            print(response)
+            
         }.resume()
     }
     func fetchClientToken() {
